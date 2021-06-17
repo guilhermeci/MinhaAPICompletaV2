d9f6252793195eb26b38589065158ff0c3e236c9
Guilherme Lopes <guilhermeci@gmail.com>
2021-06-16 21:26:06 -0300

    Aula: Finalizando a autorização com JWT

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -77,7 +77,7 @@ namespace DevIO.Api.Controllers
             NotificarErro("Usuario ou Senha incorretos");
             return CustomResponse(loginUser);
         }
-        private async Task<string> GerarJwt(string email)
+        private async Task<LoginResponseViewModel> GerarJwt(string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
             var claims = await _userManager.GetClaimsAsync(user);
@@ -109,7 +109,17 @@ namespace DevIO.Api.Controllers
                                                         new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature                                        )
             }) ;
             var encodedToken = tokenHandler.WriteToken(token);
-            return encodedToken;
+
+            var response = new LoginResponseViewModel {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UserToken = new UserTokenViewModel 
+                { 
+                    Id = user.Id,
+                    Email = user.Email, 
+                    Claims = claims.Select(c => new ClaimViewModel { Type =c.Type, Value = c.Value })}
+            };
+            return response;
         }
 
         private static long ToUnixEpochDate(DateTime date)
