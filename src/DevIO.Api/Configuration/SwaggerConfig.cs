4b6655191b346a434908388080c27e54833258b5
Guilherme Lopes <guilhermeci@gmail.com>
2021-08-04 20:20:20 -0300

    Autorização via JWT no Swagger

--- src/DevIO.Api/Configuration/SwaggerConfig.cs
+++ src/DevIO.Api/Configuration/SwaggerConfig.cs
@@ -14,7 +14,7 @@ using Microsoft.AspNetCore.Builder;
 
 namespace DevIO.Api.Configuration
 {
-
+    // ref https://stackoverflow.com/questions/43447688/setting-up-swagger-asp-net-core-using-the-authorization-headers-bearer
     public static class SwaggerConfig
     {
         public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
@@ -22,6 +22,31 @@ namespace DevIO.Api.Configuration
             services.AddSwaggerGen(c =>
             {
                 c.OperationFilter<SwaggerDefaultValues>();
+                OpenApiSecurityRequirement security = new OpenApiSecurityRequirement {};
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+                
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Insira o token JWT desta maneira : 'Bearer' [espaço] e seu token.
+                      \r\n\r\nExemplo: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement(){
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
             });
             return services;
         }
